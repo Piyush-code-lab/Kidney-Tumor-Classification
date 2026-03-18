a0386eca672c123e56f9d4c469322d47bbff5c73
Piyush-code-lab <yonkohunter48@gmail.com>
2026-03-18 21:29:02 +0530

    Base Model Initialized

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Kidney_Tumor_Classifier import logger
 from Kidney_Tumor_Classifier.pipeline.stage_01 import DataIngestionTrainingPipeline
-
+from Kidney_Tumor_Classifier.pipeline.stage_02 import PrepareBaseModelTrainingPipeline
 
 
 
@@ -10,6 +10,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started ") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed \n")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started ")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed \n")
 except Exception as e:
         logger.exception(e)
