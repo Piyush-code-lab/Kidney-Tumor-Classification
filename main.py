d94f5d545fb25383abeeef249da9353c9657dcb5
Piyush-code-lab <yonkohunter48@gmail.com>
2026-03-20 00:40:05 +0530

    model_training.py added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Kidney_Tumor_Classifier import logger
 from Kidney_Tumor_Classifier.pipeline.stage_01 import DataIngestionTrainingPipeline
 from Kidney_Tumor_Classifier.pipeline.stage_02 import PrepareBaseModelTrainingPipeline
-
+from Kidney_Tumor_Classifier.pipeline.stage_03 import ModelTrainingPipeline
 
 
 
@@ -21,6 +21,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started ")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed \n")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started ")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed \n")
 except Exception as e:
         logger.exception(e)
